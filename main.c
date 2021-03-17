29ac14f06331c04785684d66983fa928708da0f2
Akshat Patel <akshat365@gmail.com>
2021-03-17 21:49:17 +0000

    Add unit tests for invalid input validation

--- main.c
+++ main.c
@@ -1,6 +1,62 @@
-#include <stdio.h>
+#include "towers.h"
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+void noNullArguments()
+{
+	{
+	    int xCoords[] = {2,3};
+	    int yCoords[] = {1,-1};
+	    int heights[] = {4, 3};
+	    int numTowers = 2;
+	    int maxStars = -1;
+	    int *tileCounts = splittingTowers(NULL, yCoords, heights, numTowers,&maxStars);
+
+	    assert(!tileCounts);
+	    assert(maxStars == -1);
+	    //might need to free tileCounts
+	    tileCounts = splittingTowers(xCoords, NULL, heights, numTowers, &maxStars);
+	    assert(!tileCounts);
+	    assert(maxStars == -1);
+	    tileCounts = splittingTowers(xCoords, yCoords, NULL, numTowers, &maxStars);
+	    assert(!tileCounts);
+	    assert(maxStars == -1);
+	    tileCounts = splittingTowers(xCoords, yCoords, NULL, numTowers, &maxStars);
+	    assert(!tileCounts);
+	    assert(maxStars == -1);
+	    tileCounts = splittingTowers(xCoords, yCoords, heights, numTowers, NULL);
+	    assert(!tileCounts);
+	    assert(maxStars == -1);
+	}
+}
+
+void noNegNumStones()
+{
+    {
+        int xCoords[] = {2,3};
+	    int yCoords[] = {1,-1};
+	    int heights[] = {-4, 3};
+	    int numTowers = 2;
+	    int maxStars = -1;
+	    int *tileCounts = splittingTowers(xCoords, yCoords, heights, numTowers,&maxStars);
+        assert(!tileCounts);
+    }
+
+    {
+        int xCoords[] = {2,3};
+	    int yCoords[] = {1,-1};
+	    int heights[] = {-4, -3};
+	    int numTowers = 2;
+	    int maxStars = -1;
+	    int *tileCounts = splittingTowers(xCoords, yCoords, heights, numTowers,&maxStars);
+        assert(!tileCounts);
+    }
+}
+
+int main()
+{
+    noNegNumStones();
+    noNullArguments();
 }
